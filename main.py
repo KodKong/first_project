62b139d22e388b76d7596d88c250767b2abac935
Roman <muzafarovroma@list.ru>
2024-07-06 23:12:55 +0300

    1

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from config import TOKEN
 
-bot = Bot(token = '')
+bot = Bot(token = TOKEN)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
