e374f0776402616b54f33723a00051bd6dd86e04
Roman <muzafarovroma@list.ru>
2024-07-11 21:09:42 +0300

    5

--- app/client_handlers.py
+++ app/client_handlers.py
@@ -1,13 +1,19 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from app.store import get_token, get_point_list, get_points_price_list
+from config import WELCOME_PROPOSAL
 
 router = Router()
 
 @router.message(CommandStart())
-async def cmd_start(message:Message): 
+async def cmd_start(message: Message): 
+    await message.answer(str(WELCOME_PROPOSAL))
     r = get_token()
     r2 = get_point_list(r)
     r3 = get_points_price_list(r, r2['salesPoints'][0]['id'])
     await message.answer(r)
+
+@router.callback_query(F.startwith())
+async def cmd_start(callback: CallbackQuery):
+    r = get_token()
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import logging
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from app.client_handlers import router
+from app.database.models import async_main
 
-bot = Bot(token = TOKEN)
-dp = Dispatcher()
 
 async def main(): 
+    await async_main()
+    bot = Bot(token = TOKEN)
+    dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
